73c3e88b148ef924af68ca9de051d45c4b97eb14
Zhipeng Jia <zhipeng.jia@outlook.com>
2016-09-09 23:48:17 +0800

    Change app name

--- app/src/main/java/com/ihandy/a2013011373/newsapp/MainActivity.java
+++ app/src/main/java/com/ihandy/a2013011373/newsapp/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_title);
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
